1858e074ad872b68da70e9dd1367bb053542fa03
Indika Piyasena <indika.piyasena@gmail.com>
2013-09-26 19:18:58 +1000

    disabled plugins and fileTemplates added

---
+++ fileTemplates/Python Class.py
@@ -0,0 +1,19 @@
+__author__ = "Indika Piyasena"
+
+class NewClass:
+    def __init__(self):
+        pass
+
+    def process(self):
+        pass
+
+    def log(self):
+        pass
+        
+if __name__ == "__main__":
+    print "Running NewClass in stand-alone-mode"
+
+    new_class = NewClass()
+    new_class.process()
+
+    new_class.log()
